608ea6d44f00c590a432eaac5a1c5907f7ce421c
Andrey Потыкун <5a.potykun@gmail.com>
2023-09-12 12:08:41 +0200

    almost end

--- src/js/app.js
+++ src/js/app.js
@@ -12,7 +12,7 @@
 */
 
 // Увімкнути/вимкнути FLS (Full Logging System) (в роботі)
-window['FLS'] = true;
+window["FLS"] = false;
 
 // Підключення основного файлу стилів
 import "../scss/style.scss";
@@ -30,7 +30,7 @@ flsFunctions.isWebp();
 /* Додавання loaded для HTML після повного завантаження сторінки */
 // flsFunctions.addLoadedClass();
 /* Модуль для роботи з меню (Бургер) */
-// flsFunctions.menuInit();
+flsFunctions.menuInit();
 /* Враховування плаваючої панелі на мобільних пристроях при 100vh */
 // flsFunctions.fullVHfix();
 
@@ -39,7 +39,7 @@ flsFunctions.isWebp();
 Документація: https://template.fls.guru/template-docs/modul-spojlery.html
 Сніппет (HTML): spollers
 */
-// flsFunctions.spollers();
+flsFunctions.spollers();
 
 /*
 Модуль "Таби"
@@ -224,7 +224,7 @@ import * as flsScroll from "./files/scroll/scroll.js";
 
 /* Динамічний адаптив */
 // Документація: https://template.fls.guru/template-docs/dinamicheskij-adaptiv.html
-// import "./libs/dynamic_adapt.js";
+import "./libs/dynamic_adapt.js";
 
 /* Форматування чисел */
 // import './libs/wNumb.min.js';
--- src/js/files/script.js
+++ src/js/files/script.js
@@ -4,25 +4,174 @@ import { isMobile } from "./functions.js";
 import { flsModules } from "./modules.js";
 //=================== SubMenu ==================
 
-let openMenu = document.querySelector('.menu__link_sub');
-let subList = document.querySelector('.link-sub');
-openMenu.onclick = () => {
-	console.log('some');
-	subList.classList.toggle('hover');
-}
+let openMenu = document.querySelector(".menu__link_sub");
+let subList = document.querySelector(".link-sub");
+openMenu.onclick = (event) => {
+   subList.classList.toggle("hover");
+   document.querySelector("body").addEventListener("click", function removeClassOpen(event) {
+      if (document.querySelector("main").contains(event.target)) {
+         subList.classList.remove("hover");
+         document.querySelector("header").removeEventListener("click", removeClassOpen);
+      }
+   });
+};
 
 //=================== infinity running string  ==================
 
-let InfStr = document.querySelector('[data-infinText]');
+// // let InfStr = document.querySelector("[data-infinText]");
+// let infScrollWord = document.querySelectorAll("[data-infinText]");
+// // console.log(InfStr);
+// function infSring(str) {
+//    while (str.offsetWidth * 2 < window.innerWidth) {
+//       // console.dir(str.textContent);
+//       str.innerHTML += str.textContent;
+//       // console.log(str.clientWidth);
+//    }
+//    str.after(str.cloneNode(true));
+// }
+// // infSring(infScrollWord[0]);
+// // infSring(infScrollWord[1]);
+// console.dir(infScrollWord[1].offsetWidth);
+// infScrollWord.forEach((e) => {
+//    // console.log(e);
+//    infSring(e);
+// });
 
+function expandSpanText() {
+   const spanElements = document.querySelectorAll("[data-infinText]");
 
+   if (spanElements.length === 0) {
+      return; // Exit if no span elements are found
+   }
 
-function infSring(str) {
-	while ((str.clientWidth * 2) < window.innerWidth) {
-		// console.dir(str.textContent);
-		str.innerHTML += str.textContent;
-		// console.log(str.clientWidth);
-	}
-	str.after(str.cloneNode(true))
+   const screenWidth = window.innerWidth;
+
+   spanElements.forEach((spanElement) => {
+      let spanWidth = spanElement.clientWidth;
+      // console.log(spanWidth);
+
+      // Check if the current text width is less than the screen width
+      if (spanWidth < screenWidth) {
+         const currentText = spanElement.textContent.trim();
+         spanElement.textContent = currentText + " " + currentText;
+
+         // Call the function recursively to keep adding text until the width exceeds the screen width
+         expandSpanText();
+      }
+   });
 }
-infSring(InfStr);
+
+// Call the function initially
+expandSpanText();
+
+// Call the function initially
+expandSpanText();
+
+//=================== cardOpen ===================
+
+let cardButton = document.querySelector(".bottom-header__cart");
+cardButton.onclick = () => {
+   cardButton.classList.toggle("open");
+
+   const close = document.querySelector(".bottom-header__cart--open-header-title").nextElementSibling;
+   close.onclick = () => {
+      cardButton.classList.remove("open");
+   };
+   const cardFon = document.querySelector(".bottom-header__cart--open");
+   cardFon.onclick = (EventTarget) => {
+      if (EventTarget.target.className === "bottom-header__cart--open") {
+         cardButton.classList.remove("open");
+      }
+   };
+};
+//=================== animationApearScrool ===================
+
+// function onEntry(entry) {
+//    entry.forEach((change) => {
+//       if (change.isIntersecting) {
+//          change.target.classList.add("element-show");
+//       }
+//    });
+// }
+
+// let options = {
+//    threshold: [0.5],
+//    rootMargin: "50px",
+// };
+// let observer = new IntersectionObserver(onEntry, options);
+// let elements = document.querySelectorAll("[data-animAppear]");
+
+// for (let elm of elements) {
+//    observer.observe(elm);
+// }
+
+const animItem = document.querySelectorAll("[data-animAppear]");
+if (animItem.length > 0) {
+   window.addEventListener("scroll", animOnScroll);
+   function animOnScroll() {
+      animItem.forEach((elem) => {
+         const animItem = elem;
+         const animItemHeight = animItem.offsetHeight;
+         const animItemOffset = offset(animItem).top;
+         const animStart = 4;
+         let animItemPoint = window.innerHeight - animItemHeight / animStart;
+         if (animItemHeight > window.innerHeight) {
+            animItemPoint = window.innerHeight - window.innerHeight / animStart;
+         }
+         if (pageYOffset > animItemOffset - animItemPoint && pageYOffset < animItemOffset + animItemHeight) {
+            animItem.classList.add("element-show");
+         } else {
+            if (!animItem.hasAttribute("data-animAppear-NoHide")) {
+               animItem.classList.remove("element-show");
+            }
+         }
+      });
+   }
+   function offset(el) {
+      const rect = el.getBoundingClientRect(),
+         scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+         scrollTop = window.pageYOffset || document.documentElement.scrolltop;
+      return { top: rect.top + scrollTop, left: rect.left + scrollLeft };
+   }
+   animOnScroll();
+}
+
+//=================== text scrool when we scroll ===================
+
+function areElementsVisible(scrollElements) {
+   const windowHeight = window.innerHeight || document.documentElement.clientHeight;
+   const windowWidth = window.innerWidth || document.documentElement.clientWidth;
+
+   const isVisible = [];
+   for (let i = 0; i < scrollElements.length; i++) {
+      const rect = scrollElements[i].getBoundingClientRect();
+      const vertInView = rect.top <= windowHeight && rect.bottom >= 0;
+      const horInView = rect.left <= windowWidth && rect.right >= 0;
+      isVisible.push({ visible: vertInView && horInView, scrollY: rect.top });
+   }
+
+   return isVisible;
+}
+
+const scrollElements = document.querySelectorAll("[data-scrollText]");
+
+window.addEventListener("scroll", () => {
+   const visibilityArray = areElementsVisible(scrollElements);
+   for (let i = 0; i < visibilityArray.length; i++) {
+      if (visibilityArray[i].visible) {
+         const scrolledPixels = (visibilityArray[i].scrollY / window.innerHeight) * 200 - 150;
+
+         const currentElement = scrollElements[i];
+         currentElement.style.transform = `translateX(${scrolledPixels}px)`;
+         // console.log(`Element ${i + 1} is visible on the screen! Scrolled ${scrolledPixels}px.`);
+         // Perform any desired actions for each visible element here
+      }
+   }
+});
+
+//=================== scrolll to top ===================
+
+const arrowToTop = document.querySelector(".bottom-foot__item--arr");
+arrowToTop.onclick = () => {
+   window.scrollTo(0, 0);
+};
